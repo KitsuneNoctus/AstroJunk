8b0a36bd1f0a71416d03c7e9941e7b4d9cb5eabd
Henry Calderon <theceintiant@gmail.com>
2020-06-04 21:47:14 -0700

    Starting a project

--- AstroJunk/GameScene.swift
+++ AstroJunk/GameScene.swift
@@ -10,80 +10,129 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+  
     
     override func didMove(to view: SKView) {
+        //MARK: Background
+        let space = SKSpriteNode(imageNamed: "space_background")
+        space.size = CGSize(width: self.frame.width, height: self.frame.height)
+        self.addChild(space)
+        createShip()
+        createMeteor()
+        createBeam()
+        createJunk()
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+    //MARK: Create Ship
+    func createShip(){
+        let ship = SKSpriteNode(imageNamed: "playerShip")
+        ship.size = CGSize(width: 64, height: 64)
+        if let view = self.view {
+            ship.position = CGPoint(x: view.bounds.width/2, y: view.bounds.height/5)
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        ship.zPosition = 2
+        self.addChild(ship)
+        
+        //Ship Move
+        let moveRight = SKAction.moveTo(x: 300, duration: 5)
+        let moveLeft = SKAction.moveTo(x: -300, duration: 5)
+        let backNForth = SKAction.sequence([moveRight, moveLeft])
+        let alwaysBNF = SKAction.repeatForever(backNForth)
+        ship.run(alwaysBNF)
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+    //MARK: Create Meteor
+    func createMeteor(){
+        //Meteor
+        let meteor = SKSpriteNode(imageNamed: "meteorGrey_med1")
+        meteor.size = CGSize(width: 40, height: 40)
+        meteor.position = CGPoint(x: 200, y: 300)
+        meteor.zPosition = 2
+        self.addChild(meteor)
+        
+        //Meteor Move
+        let turn = SKAction.rotate(byAngle: 20, duration: 1)
+        let spin = SKAction.repeatForever(turn)
+        let moveDown = SKAction.move(to: CGPoint(x: 0, y: -700), duration: 4)
+        let movement = SKAction.group([spin, moveDown])
+        
+        //        let meteorEvent = SKAction.sequence([movement, gone])
+        meteor.run(movement)
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    //MARK: Create Beam
+    func createBeam(){
+        
+        let beam = SKSpriteNode(imageNamed: "beam")
+        beam.size = CGSize(width: 20, height: 20)
+        beam.position = CGPoint(x: 100, y: 500)
+        beam.zPosition = 2
+        self.addChild(beam)
+        
+        let moveRight = SKAction.moveTo(x: 300, duration: 5)
+        let moveLeft = SKAction.moveTo(x: -300, duration: 5)
+        let backNForth = SKAction.sequence([moveRight, moveLeft])
+        let alwaysBNF = SKAction.repeatForever(backNForth)
+        beam.run(alwaysBNF)
     }
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+    //MARK: Create Junk
+    func createJunk(){
+        let junk1 = SKSpriteNode(imageNamed: "wingGreen_6")
+        junk1.size = CGSize(width: 30, height: 30)
+        junk1.position = CGPoint(x: 300, y: 450)
+        junk1.zPosition = 2
+        self.addChild(junk1)
+        
+        //Debris Move
+        let turn1 = SKAction.rotate(byAngle: 2, duration: 1)
+        let turn2 = SKAction.rotate(byAngle: -2, duration: 1)
+        let turning = SKAction.sequence([turn1,turn2])
+        let repetition = SKAction.repeatForever(turning)
+        let going = SKAction.move(to: CGPoint(x: -400, y: -500), duration: 15)
+        let ending = SKAction.removeFromParent()
+        let repetition2 = SKAction.sequence([going,ending])
+        let traverse = SKAction.group([repetition,repetition2])
+        junk1.run(traverse)
+        
     }
+    
+    func rotateMovemet(node: SKSpriteNode){
+//        node.run(groupAction)
+    }
+    
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
+//    func touchDown(atPoint pos : CGPoint) {
+//
+//    }
+//
+//    func touchMoved(toPoint pos : CGPoint) {
+//
+//    }
+//
+//    func touchUp(atPoint pos : CGPoint) {
+//
+//    }
+//
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//
+//    }
+//
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//
+//    }
+//
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//    }
+//
+//    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+//    }
 }
